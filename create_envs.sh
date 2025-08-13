754b697f3db439560544bb75c12b42a036a56c41
UBinKitte <UBinPost@aol.com>
2025-08-13 12:49:00 +0900

    First commit

---
+++ create_envs.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+config_dirs=(
+    "alacritty"
+    "labwc"
+    "swaylock"
+    "waybar"
+)
+
+for name in "${config_dirs[@]}"; do
+    src="$HOME/dotfiles/.config/$name"
+    dest="$HOME/.config/$name"
+
+    if [ -e "$dest" ] || [ -L "$dest" ]; then
+        echo "$dest already exists!"
+    else
+        ln -s "$src" "$dest"
+        echo "Created: $dest -> $src"
+    fi
+done
