46c376a5fe3034d24473c28755b834a07099b319
NP-F <supernetaparty.1@gmail.com>
2025-09-01 20:49:29 +0900

    still  working now

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,1 +1,3 @@
 require("config.lazy")
+
+vim.opt.number = true
---
+++ bootstrap.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if ! command -v ansible; then
+	sudo apt update
+	sudo apt install -y ansible git python3
+fi
+
+ansible-playbook -i hosts.ini setup.yml
