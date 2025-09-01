de7b727096e276251683910211e5a9dbaf5affa5
NP-F <supernetaparty.1@gmail.com>
2025-09-01 21:07:23 +0900

    Update: better bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
+REPO="https://github.com/NP-F/dotfiles.git"
+DEST="$HOME/dotfiles"
+
+sudo apt update
+
 if ! command -v ansible; then
-	sudo apt update
-	sudo apt install -y ansible git python3
+	sudo apt install -y ansible
+fi
+
+if ! command -v git; then
+	sudo apt install -y git
+fi
+
+if ! [ -d "$DEST" ]; then
+	git clone "$REPO" "$DEST"
+	cd "$DEST"
+else
+	cd "$DEST"
+	git pull
 fi
 
 ansible-playbook -i hosts.ini setup.yml -k
